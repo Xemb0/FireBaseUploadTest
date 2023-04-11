f772998d50498b2b3cf417f5458e9354dab3657e
XEMBO <nishadvishal937@gmail.com>
2023-04-11 15:32:38 +0530

    FIREBASE WORKING UPLODE DATA

---
+++ app/src/main/java/com/example/firebasetesting/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.firebasetesting;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Document;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+firestore=FirebaseFirestore.getInstance();
+        Map<String,Object> user = new HashMap<>();
+        user.put("Amit", "745");
+        user.put("su", "745");
+        user.put("hu", "745");
+
+        firestore.collection("user").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "FAli our", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
